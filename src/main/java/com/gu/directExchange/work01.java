aa37fb82abf9e1fc943aa2b0750e66019d7f3808
guhuan <2371906989@qq.com>
2022-03-09 17:28:50 +0800

    直接交换机

---
+++ src/main/java/com/gu/directExchange/work01.java
@@ -0,0 +1,24 @@
+package com.gu.directExchange;
+
+import com.gu.utils.RabbitMqUtils;
+import com.rabbitmq.client.CancelCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+
+public class work01 {
+    private static final String EXCHANGE_NAME = "directExchanges";
+
+    public static void main(String[] args) throws Exception {
+        Channel channel = RabbitMqUtils.getChannel();
+        String QUEUE_NAME = "Q1";
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,"blank");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery)->{
+            String message = new String(delivery.getBody(), "UTF-8");
+            message=QUEUE_NAME+"接收绑定键:"+delivery.getEnvelope().getRoutingKey()+",消息:"+message;
+            System.out.println(message);
+        };
+        channel.basicConsume(QUEUE_NAME,true,deliverCallback, CancelCallback->{});
+    }
+}
